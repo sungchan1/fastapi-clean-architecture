12dc7e99cf576a1fa0a5bbdf3e82f6db554a7306
sungchan <sungchan1@naver.com>
2024-12-25 18:37:10 +0900

    feat: Implement user registration endpoint

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+
+from user.interface.controllers.user_controller import router as user_routers
 
 app = FastAPI()
+app.include_router(user_routers)
 
-@app.get("/")
-def hello():
-    return {"Hello": "FastAPI"}
-
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+        request: Request,
+        exc: RequestValidationError
+):
+    return JSONResponse(
+        status_code=400,
+        content=exc.errors(),
+    )
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", reload=True)
---
+++ user/interface/controllers/user_controller.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+from user.application.user_service import UserService
+
+router = APIRouter(prefix="/users")
+
+class CreateUserBody(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+@router.post("", status_code=201)
+def create_user(user: CreateUserBody):
+    user_service = UserService()
+    create_user = user_service.create_user(
+        name=user.name,
+        email=user.email,
+        password=user.password
+    )
+    return create_user
